39fae25251648723991ec6dae8322815ce5a3b38
国星 孙 <tcguoxing@163.com>
2022-03-16 01:24:26 +0800

    学习了el的message以及背后逻辑

--- src/components/subPage/homePage.vue
+++ src/components/subPage/homePage.vue
@@ -7,7 +7,8 @@
     <h3>Element按钮</h3>
     <ul>
       <li>
-        <el-button>这是个elButton</el-button>
+        <el-button @click="showConfirmDialog">这是个elButton</el-button>
+        <el-button @click="showMessage">这是个showMessage</el-button>
       </li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
     </ul>
@@ -31,12 +32,44 @@
 </template>
 
 <script>
+/* eslint-disable */
+// import { ElMessageBoxOptions } from '@/../element-ui/types/message-box'
+
 export default {
   name: 'homePage',
   props: {
     msg: String
-  }
+  },
+  methods: {
+  showConfirmDialog(){
+    let thisConfirm = this.$confirm("请问是否要确定？","注意", {
+     confirmButtonText: "确定啦",
+    //  confirmButtonLoading: false,
+     callback: (action, instance) => {
+       instance.confirmButtonLoading = true
+       setTimeout(() => {
+         instance.confirmButtonLoading = false
+       }, 2000)
+       console.log('this callback action', action)
+       console.log('this callback instance', instance)
+     },
+    //  beforeClose: (action, instance, _) => {
+    //   //  instance.confirmButtonLoading = true
+    //    console.log('this beforeClass action', action)
+    //    console.log('this beforeClass instance', instance)
+    //  }
+    })
+    thisConfirm.then((val) => {
+      console.log('thisConfirmThen:', val)
+    })
+    console.log('thisConfirm type: ', thisConfirm)
+    },
+    showMessage() {
+      this.$Message("这个内容是","", {text: "内容", type: "warn", onClose: ()=> {console.log('我关了哟')}})
+    },
+  },
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.js
+++ src/main.js
@@ -1,18 +1,24 @@
+/* eslint-disable */
 import Vue from 'vue'
 import App from './App.vue'
-import element from 'element-ui'
-// import { Button, MessageBox } from 'element-ui'
+import element, { MessageBox } from 'element-ui'
 import router from '@/router/index.js'
-/* eslint-disable */
+// const { ElMessageBoxComponent } = require('@/../node_modules/element-ui/types/message-box.d.ts') 
+// import { ElMessageBoxComponent } from '@/../node_modules/element-ui/types/message-box.d.ts'
+
 // extends中注释掉了 "eslint:recommended"，为了调试顺利。上边这行可以把整个文件的错误忽略掉。
 
-const allElementNames = [ 'CollapseTransition', 'Loading', 'Pagination', 'Dialog', 'Autocomplete', 'Dropdown', 'DropdownMenu', 'DropdownItem', 'Menu', 'Submenu', 'MenuItem', 'MenuItemGroup', 'Input', 'InputNumber', 'Radio', 'RadioGroup', 'RadioButton', 'Checkbox', 'CheckboxButton', 'CheckboxGroup', 'Switch', 'Select', 'Option', 'OptionGroup', 'Button', 'ButtonGroup', 'Table', 'TableColumn', 'DatePicker', 'TimeSelect', 'TimePicker', 'Popover', 'Tooltip', 'MessageBox', 'Breadcrumb', 'BreadcrumbItem', 'Form', 'FormItem', 'Tabs', 'TabPane', 'Tag', 'Tree', 'Alert', 'Notification', 'Slider', 'Icon', 'Row', 'Col', 'Upload', 'Progress', 'Spinner', 'Message', 'Badge', 'Card', 'Rate', 'Steps', 'Step', 'Carousel', 'Scrollbar', 'CarouselItem', 'Collapse', 'CollapseItem', 'Cascader', 'ColorPicker', 'Transfer', 'Container', 'Header', 'Aside', 'Main', 'Footer', 'Timeline', 'TimelineItem', 'Link', 'Divider', 'Image', 'Calendar', 'Backtop', 'InfiniteScroll', 'PageHeader', 'CascaderPanel', 'Avatar', 'Drawer', 'Popconfirm', 'Skeleton', 'SkeletonItem', 'Empty', 'Descriptions', 'DescriptionsItem', 'Result']
+const allElementNames = [ 'CollapseTransition', 'Loading', 'Pagination', 'Dialog', 'Autocomplete', 'Dropdown', 'DropdownMenu', 'DropdownItem', 'Menu', 'Submenu', 'MenuItem', 'MenuItemGroup', 'Input', 'InputNumber', 'Radio', 'RadioGroup', 'RadioButton', 'Checkbox', 'CheckboxButton', 'CheckboxGroup', 'Switch', 'Select', 'Option', 'OptionGroup', 'Button', 'ButtonGroup', 'Table', 'TableColumn', 'DatePicker', 'TimeSelect', 'TimePicker', 'Popover', 'Tooltip', 'Breadcrumb', 'BreadcrumbItem', 'Form', 'FormItem', 'Tabs', 'TabPane', 'Tag', 'Tree', 'Alert', 'Notification', 'Slider', 'Icon', 'Row', 'Col', 'Upload', 'Progress', 'Spinner', 'Message', 'Badge', 'Card', 'Rate', 'Steps', 'Step', 'Carousel', 'Scrollbar', 'CarouselItem', 'Collapse', 'CollapseItem', 'Cascader', 'ColorPicker', 'Transfer', 'Container', 'Header', 'Aside', 'Main', 'Footer', 'Timeline', 'TimelineItem', 'Link', 'Divider', 'Image', 'Calendar', 'Backtop', 'InfiniteScroll', 'PageHeader', 'CascaderPanel', 'Avatar', 'Drawer', 'Popconfirm', 'Skeleton', 'SkeletonItem', 'Empty', 'Descriptions', 'DescriptionsItem', 'Result']
 console.log('confirm: ', Object.getOwnPropertyDescriptors(element.MessageBox.confirm))
+console.log('message: ', Object.getOwnPropertyDescriptors(element.MessageBox))
+// console.log('message: ', Object.getOwnPropertyDescriptors(ElMessageBoxComponent))
 Vue.config.productionTip = false
 for (const [key, val] of allElementNames.entries()) {
   Vue.use(element[val])
 }
+Vue.component(element.MessageBox.name, MessageBox)
 Vue.prototype.$Message = element.Message
+Vue.prototype.$confirm = element.MessageBox.confirm
 
 new Vue({
   router,
