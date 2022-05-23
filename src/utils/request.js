e2c1f05f1e530e5433fd923ae2fe7620cf308a76
国星 孙 <tcguoxing@163.com>
2022-05-23 23:32:04 +0800

    添加axios封装

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 // 这是一个网站学到的封装，网址为：https://www.cnblogs.com/panax/p/10942889.html
 
-// generate one axios instance
-var instance = axios.create({ timeout: 1000 * 12 });
-// set post request
+// 创建一个axios实例
+var instance = axios.create({ timeout: 1000 * 12 });// 创建一个axios实例
+// 设置一个post请求头
 instance.defaults.headers.post["Content-Type"] =
   "application/x-www-form-urlencode";
 
@@ -11,19 +11,17 @@ instance.defaults.headers.post["Content-Type"] =
 instance.interceptors.request.use(
   (config) => {
     // 登录流程控制中，根据本地是否存在token判断用户的登录情况
-    // 但是即使token存在，也有可能token是过期的，所以在每次的请求头
-    中携带token;
-    // 后台根据携带的token判断用户的登录情况，并返回给我们对应的状态
-    码;
+    // 但是即使token存在，也有可能token是过期的，所以在每次的请求头中携带token;
+    // 后台根据携带的token判断用户的登录情况，并返回给我们对应的状态码;
     // 而后我们可以在响应拦截器中，根据状态码进行一些统一的操作。
-    const token = store.state.token;
+    const token = store.state.token; // store莫非是vue中的状态管理器
     token && (config.headers.Authorization = token);
     return config;
   },
   (error) => Promise.error(error)
 );
-// 响应拦截器
 
+// 响应拦截器，我的理解是响应返回后的解决内容。
 instance.interceptors.response.use(
   // 请求成功
   (res) => (res.status === 200 ? Promise.resolve(res) : Promise.reject(res)),
